1f208369f7e992cd0683f01767691b41f4bcae0c
Adam Conder <adamdconder@gmail.com>
2018-01-22 11:06:33 +0000

    Add function to greet

--- src/main/scala/app/GreetingsApplication.scala
+++ src/main/scala/app/GreetingsApplication.scala
@@ -2,6 +2,10 @@ package scala.app
 
 object GreetingsApplication extends App {
 
-  println("Hello!")
+  def greet(name : String) : Unit = println("Hello " + name)
+
+  val name = "Adam"
+
+  greet(name)
 
 }
