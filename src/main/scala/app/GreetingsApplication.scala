0e354cb907c31e3e8c25b88b542460ee1355852b
Adam Conder <adamdconder@gmail.com>
2018-01-22 13:44:33 +0000

    Add conditional expression

--- src/main/scala/app/GreetingsApplication.scala
+++ src/main/scala/app/GreetingsApplication.scala
@@ -4,7 +4,13 @@ import scala.io.StdIn
 
 object GreetingsApplication extends App {
 
-  def greet(name : String) : Unit = println("Hello " + name)
+  def greet(name : String) : Unit = {
+    if (name == "adam") {
+      println("You don't get a hello.")
+    } else {
+      println("Hello " + name)
+    }
+  }
 
   val name = StdIn.readLine("What is your name? ")
 
