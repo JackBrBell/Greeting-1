99df880561e1cabb7e4e9a7f5f2623d720651bdc
Adam Conder <adamdconder@gmail.com>
2018-01-22 11:48:57 +0000

    Add standard input

--- src/main/scala/app/GreetingsApplication.scala
+++ src/main/scala/app/GreetingsApplication.scala
@@ -1,10 +1,12 @@
 package scala.app
 
+import scala.io.StdIn
+
 object GreetingsApplication extends App {
 
   def greet(name : String) : Unit = println("Hello " + name)
 
-  val name = "Adam"
+  val name = StdIn.readLine("What is your name? ")
 
   greet(name)
 
